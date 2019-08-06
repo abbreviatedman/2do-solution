b232c7d27b954f8104549881d2b6e697e8dcd9c0
Colin Jaffe <balloonasaurus@gmail.com>
2019-08-06 10:06:32 -0400

    Adds second array/tuple system and updates base functions to match

--- main.js
+++ main.js
@@ -7,10 +7,31 @@ const todos = [
   `Add dates to todos.`,
 ];
 
+const completed = [
+  false,
+  false,
+  false,
+]
+
+// Or the ALTERNATE method:
+/*
+const todos = [
+  [`Be able to mark todos "done".`, false],
+  [`Allow user interaction through the DOM`, false],
+  [`Add dates to todos.`, false],
+]
+
+*/
+
 // A function that prints a todo.
 // For now, just console log it!
 function printTodo(todo) {
-  console.log(todo);
+  const ol = document.querySelector('.todo-list');
+  
+  const li = document.createElement('li');
+  li.innerText = todo;
+
+  ol.appendChild(li);
 }
 
 // A function that prints everything on our todo list, INDIVIDUALLY.
@@ -27,9 +48,22 @@ function printList() {
 // Where on the list should we add it?
 function addTodo(todo) {
   todos.push(todo);
+  completed.push(todo);
+
+  /*
+    Using tuples:
+
+    todos.push([todo, false]);
+  */
 }
 
 // A function that removes an item at a given index from our todo list.
 function removeTodo(i) {
   todos.splice(i, 1);
+
+  // Unneeded if using tuples:
+  completed.splice(i, 1);
 }
+
+
+
